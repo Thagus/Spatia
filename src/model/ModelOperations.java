770b41b61553871cd33ab51d4b9fede3e7ba5ef3
Thagus <agu.alcarin@gmail.com>
2016-09-05 14:13:01 -0500

    Term search and query evaluation

--- src/model/ModelOperations.java
+++ src/model/ModelOperations.java
@@ -1,11 +1,19 @@
 package model;
 
 import dataObjects.Document;
+import dataObjects.DocumentTerm;
+import dataObjects.Term;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import utilities.Tokenizer;
 
+import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,27 +23,24 @@ import java.util.Map;
 public class ModelOperations {
     private ModelDatabase db;
 
-    private PreparedStatement stGetTFIDF;
+    private PreparedStatement stCalculateTFIDF;
+    private PreparedStatement stGetDocumentTerm;
 
     protected ModelOperations(Connection connection, ModelDatabase db) throws SQLException{
         this.db = db;
-        stGetTFIDF = connection.prepareStatement("SELECT tfidf FROM SPATIA.TERMS WHERE idDoc=? AND term=?");
-
+        stCalculateTFIDF = connection.prepareStatement("UPDATE SPATIA.TERMS a SET a.tfidf=(SELECT b.idf*a.tf FROM SPATIA.IDF b WHERE a.term=b.term)");
+        stGetDocumentTerm = connection.prepareStatement("SELECT d.idDoc, d.title, d.journal, t.tf, t.tfidf FROM SPATIA.DOCUMENT d NATURAL JOIN SPATIA.TERMS t WHERE t.term=?");
     }
 
     public ArrayList<Document> evaluateQuery(String query){
-        String lowercase = query.toLowerCase();
-
-        //Remove numbers
-        lowercase = lowercase.replaceAll("\\d","");
-        //Split at spaces or punctuation
-        String[] spplited = lowercase.split("(\\s+)|\\p{Punct}");
+        String[] splited = Tokenizer.tokenizeString(query);
 
         HashMap<String, Integer> wordCount = new HashMap<>();
-        HashMap<String, Float> wordTFIDF = new HashMap<>();
+        HashMap<String, Double> wordTFIDF = new HashMap<>();
+        HashMap<Integer, ArrayList<Term>> documentTerms = new HashMap<>();
         ArrayList<Document> searchResult = new ArrayList<>();
 
-        for(String word : spplited){
+        for(String word : splited){
             if(word.length()<=2)    //Filter void(0) words, and those with length 1 or 2
                 continue;
 
@@ -49,25 +54,77 @@ public class ModelOperations {
 
         //Calculate tfidf for query
         for(Map.Entry<String, Integer> entry : wordCount.entrySet()){
-            wordTFIDF.put(entry.getKey(), entry.getValue()*db.opIDF.getTermIDF(entry.getKey()));
+            double tfidfQuery = entry.getValue()*db.opIDF.getTermIDF(entry.getKey());
+            wordTFIDF.put(entry.getKey(), tfidfQuery);
+
+            //Get documents that contain terms from query
+            ArrayList<Term> docTerms = db.opTerm.getDocumentsContainingTerm(entry.getKey());
+            for(Term t : docTerms){
+                /*
+                if(documentTerms.containsKey(t.getIdDoc())){
+                    documentTerms.get(t.getIdDoc()).add(t);
+                } else{
+                    ArrayList<Term> newArr = new ArrayList<>();
+                    newArr.add(t);
+                    documentTerms.put(t.getIdDoc(), newArr);
+                }*/
+                documentTerms.putIfAbsent(t.getIdDoc(), new ArrayList<Term>());  //If there is no entry for IdDoc, put a new entry
+                t.setSimilarity(t.getTfidf()*tfidfQuery);                       //Calculate term similarity to query term
+                documentTerms.get(t.getIdDoc()).add(t);                          //Put the current Term in their corresponding ArrayList in the HashMap
+            }
         }
 
-        //Get documents that contain terms in query
+        //Calculate similarity for documents
+        for(Map.Entry<Integer, ArrayList<Term>> entry : documentTerms.entrySet()){
+            //Get document object by idDoc
 
-        //Calculate similarity
+            //Calculate similarity by getting the current and adding the new one
 
-        //Sort the according similarity
+            //Put it on the ArrayList
+        }
+
+        //Sort the ArrayList
+        Collections.sort(searchResult);
 
         //Return
         return searchResult;
     }
 
-    public void termSearch(String term){
+    public ObservableList<DocumentTerm> termSearch(String term){
+        ObservableList<DocumentTerm> searchResult = FXCollections.observableArrayList();
 
+        //Natural join between Term and idf and document, filter by term
+        try {
+            stGetDocumentTerm.clearParameters();
+            stGetDocumentTerm.setString(1, term);
+
+            ResultSet rs = stGetDocumentTerm.executeQuery();
+            boolean check = false;
+
+            while (rs.next()){
+                check = true;
+                searchResult.add(new DocumentTerm(rs.getInt("idDoc"), rs.getString("title"), rs.getString("journal"), rs.getInt("tf"), rs.getDouble("tfidf")));
+            }
+
+            if(!check){
+                JOptionPane.showMessageDialog(null,"There are no documents that join the term: \"" + term + "\"", "Alert!", JOptionPane.ERROR_MESSAGE);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, e.toString(), "Error getting document and term", JOptionPane.ERROR_MESSAGE);
+        }
+        return searchResult;
     }
 
 
     public void calculateTFIDFs(){
-
+        try{
+            stCalculateTFIDF.executeUpdate();
+        } catch(SQLException e){
+            //Unhandled error
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, e.toString(), "Error adding term to IDF", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
