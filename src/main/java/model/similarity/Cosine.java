446aacd5edbcd755d4abeceab127aaf08f3436c6
Thagus <agu.alcarin@gmail.com>
2016-09-26 22:50:30 -0500

    Cosine correction

--- src/main/java/model/similarity/Cosine.java
+++ src/main/java/model/similarity/Cosine.java
@@ -21,7 +21,7 @@ public class Cosine extends Similarity {
     public Cosine(Connection connection) throws SQLException {
         super("Cosine", connection);
 
-        calculateCosine = connection.prepareStatement("SELECT i.idDoc, SUM((q.weight*i.weight)/((SELECT SUM(q1.weight) FROM QUERY q1)*(SELECT SUM(i2.weight) FROM SPATIA.INVERTEDINDEX i2 WHERE i2.idDoc=i.idDoc))) as sim\n" +
+        calculateCosine = connection.prepareStatement("SELECT i.idDoc, SUM((q.weight*i.weight)/SQRT((SELECT SUM(q1.weight) FROM QUERY q1)*(SELECT SUM(i2.weight) FROM SPATIA.INVERTEDINDEX i2 WHERE i2.idDoc=i.idDoc))) as sim\n" +
                 "FROM QUERY q, SPATIA.INVERTEDINDEX i\n" +
                 "WHERE q.term=i.term\n" +
                 "GROUP BY i.idDoc\n" +
