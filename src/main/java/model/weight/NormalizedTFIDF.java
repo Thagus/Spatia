07e9b8b99ee3d4663a9d8434e7d58aecaaad84d2
Thagus <agu.alcarin@gmail.com>
2016-09-26 20:39:06 -0500

    Normalized TF-IDF query

--- src/main/java/model/weight/NormalizedTFIDF.java
+++ src/main/java/model/weight/NormalizedTFIDF.java
@@ -11,7 +11,10 @@ public class NormalizedTFIDF extends Weight {
         super("Normalized TF-IDF", connection);
 
         //[(log tfij + 1) * idfj] / [Σj=1,t [(log tfij + 1) * idfj]2]
-        stCalculateWeight = connection.prepareStatement("MERGE INTO SPATIA.TERM(term,idf) " +
-                "SELECT t.term, ((log(max(i.tf) + 1)*t.idf)/(sum(power(log(i.tf + 1)*t.idf, 2)))) as weight FROM SPATIA.TERM t NATURAL JOIN SPATIA.INVERTEDINDEX i GROUP BY t.term HAVING weight>0");
+        stCalculateWeight = connection.prepareStatement("UPDATE SPATIA.INVERTEDINDEX i SET i.weight=((log(i.tf+1)*(SELECT t.idf FROM SPATIA.TERM t WHERE t.term=i.term))/\n" +
+                "(SELECT SUM(POWER(log(i2.tf+1)*t2.idf, 2)) FROM SPATIA.INVERTEDINDEX i2, SPATIA.TERM t2 WHERE i2.idDoc=i.idDoc AND t2.term=i2.term))");
+
+        stCalculateQueryWeight = connection.prepareStatement("UPDATE QUERY q SET q.weight=IFNULL(((log(q.tf+1)*(SELECT t.idf FROM SPATIA.TERM t WHERE t.term=q.term))/\n" +
+                "(SELECT SUM(POWER(log(q2.tf+1)*t2.idf, 2)) FROM QUERY q2, SPATIA.TERM t2 WHERE t2.term=q2.term)), 0)");
     }
 }
