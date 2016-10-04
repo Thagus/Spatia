7bd32270e4d79b7a17c041c8fe117ec64f219fd1
Thagus <agu.alcarin@gmail.com>
2016-10-04 15:41:49 -0500

    Similarity feedback

--- src/main/java/model/ModelOperations.java
+++ src/main/java/model/ModelOperations.java
@@ -61,7 +61,7 @@ public class ModelOperations {
         ObservableList<Document> searchResult;
 
         //Request the calculation of similarity for the query, and save the results in the searchResult list
-        searchResult = similarity.calculateSimilarity(wordCount);
+        searchResult = similarity.similarityFeedback(wordCount, 0, 0, 1);   //0,0 for default limits, and the 1 for one iteration
 
         //Sort the results by similarity, from highest to lowest
         Collections.sort(searchResult, Collections.reverseOrder());
