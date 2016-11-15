e5237bb3ea4ea1b9d06dc11df0362833c095db79
Thagus <agu.alcarin@gmail.com>
2016-11-14 19:09:48 -0600

    Basic crawling with crawler4j

---
+++ src/main/java/crawling/Crawler.java
@@ -0,0 +1,79 @@
+package crawling;
+
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import org.apache.http.Header;
+
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+
+/**
+ * Created by Thagus on 14/11/16.
+ */
+public class Crawler extends WebCrawler{
+    private static final Pattern IMAGE_EXTENSIONS = Pattern.compile(".*\\.(bmp|gif|jpg|png)$");
+    private static final Pattern SOUND_EXTENSIONS = Pattern.compile(".*\\.(mp3|ogg)$");
+    private static final Pattern COMPRESSED_EXTENSIONS = Pattern.compile(".*\\.(zip|rar|gz|tar)$");
+    private static final Pattern OTHER_EXTENSIONS = Pattern.compile(".*\\.(css|js)$");
+
+    /**
+     * You should implement this function to specify whether the given url
+     * should be crawled or not (based on your crawling logic).
+     */
+    @Override
+    public boolean shouldVisit(Page referringPage, WebURL url) {
+        String href = url.getURL().toLowerCase();
+        //Ignore the url if has the defined extensions
+        return !(IMAGE_EXTENSIONS.matcher(href).matches() ||
+                SOUND_EXTENSIONS.matcher(href).matches() ||
+                COMPRESSED_EXTENSIONS.matcher(href).matches() ||
+                OTHER_EXTENSIONS.matcher(href).matches());
+    }
+
+    /**
+     * This function is called when a page is fetched and ready to be processed
+     * by your program.
+     */
+    @Override
+    public void visit(Page page) {
+        int docid = page.getWebURL().getDocid();
+        String url = page.getWebURL().getURL();
+        String domain = page.getWebURL().getDomain();
+        String path = page.getWebURL().getPath();
+        String subDomain = page.getWebURL().getSubDomain();
+        String parentUrl = page.getWebURL().getParentUrl();
+        String anchor = page.getWebURL().getAnchor();
+
+        logger.debug("Docid: {}", docid);
+        logger.info("URL: {}", url);
+        logger.debug("Domain: '{}'", domain);
+        logger.debug("Sub-domain: '{}'", subDomain);
+        logger.debug("Path: '{}'", path);
+        logger.debug("Parent page: {}", parentUrl);
+        logger.debug("Anchor text: {}", anchor);
+
+        if (page.getParseData() instanceof HtmlParseData) {
+            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+            String text = htmlParseData.getText();
+            String html = htmlParseData.getHtml();
+            Set<WebURL> links = htmlParseData.getOutgoingUrls();
+
+            logger.debug("Text length: {}", text.length());
+            logger.debug("Html length: {}", html.length());
+            logger.debug("Number of outgoing links: {}", links.size());
+        }
+
+        Header[] responseHeaders = page.getFetchResponseHeaders();
+        if (responseHeaders != null) {
+            logger.debug("Response headers:");
+            for (Header header : responseHeaders) {
+                logger.debug("\t{}: {}", header.getName(), header.getValue());
+            }
+        }
+
+        logger.debug("=============");
+    }
+}
