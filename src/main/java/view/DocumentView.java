09ae84031329d07be632624fd8e5b0abdb339fde
Thagus <agu.alcarin@gmail.com>
2016-11-21 21:34:33 -0600

    Progress bar while loading website and total load time

--- src/main/java/view/DocumentView.java
+++ src/main/java/view/DocumentView.java
@@ -1,17 +1,16 @@
 package view;
 
 import dataObjects.Document;
+import javafx.concurrent.Worker;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.beans.property.*;
+import javafx.scene.control.*;
 
 /**
  * Created by Thagus on 05/09/16.
@@ -28,10 +27,35 @@ public class DocumentView {
         WebView webView = new WebView();
         WebEngine webEngine = webView.getEngine();
 
+        Label timeLabel = new Label();
+        timeLabel.setVisible(false);
+
+        final ProgressBar progress = new ProgressBar();
+        final LongProperty startTime = new SimpleLongProperty();
+        final LongProperty endTime = new SimpleLongProperty();
+
+        // updating progress bar using binding
+        progress.progressProperty().bind(webEngine.getLoadWorker().progressProperty());
+
+        webEngine.getLoadWorker().stateProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                    if(newValue == Worker.State.RUNNING){
+                        startTime.setValue(System.currentTimeMillis());
+                    }
+                    else if (newValue == Worker.State.SUCCEEDED) {
+                        // hide progress bar then page is ready
+                        progress.setVisible(false);
+                        endTime.setValue(System.currentTimeMillis());
+                        timeLabel.setText("Load time: " + (endTime.getValue()-startTime.getValue()) + " ms");
+                        timeLabel.setVisible(true);
+                    }
+                }
+        );
+
         webEngine.load(document.getUrl());
 
         layout.getChildren().addAll(
-                webView
+                webView, progress, timeLabel
         );
 
         stage.setScene(scene);
